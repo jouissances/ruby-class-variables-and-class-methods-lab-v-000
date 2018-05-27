945cfbd64fbe07acb6f51bc7ee7324206462456b
Janice Darikho <janicedarikho@gmail.com>
2018-05-27 10:04:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ class Song
   
   @@count = 0
   @@artists = []
+  @@genres = []
   
   def initialize
     @@count += 1
@@ -14,5 +15,9 @@ class Song
   
   def self.artists
     @@artists << @artist
+  end
+  
+  def self.genres
+    @@genres << @genres
   end
 end
